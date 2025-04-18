8fa8a71a6d209e95530814f38c292a76b666f9af
iagouvea07 <iagouvea@outlook.com>
2025-04-18 16:28:02 -0300

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json({ limit: '100mb' }));
+
+//app.use("/", router.getRouter)
+//app.use("/", router.postRouter)
+//app.use("/", router.putRouter)
+//app.use("/", router.deleteRouter)
+
+const server = app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+});
+  
+server.on('error', (err) => {
+  console.error('❌ Erro ao iniciar o servidor:', err.message);
+});
